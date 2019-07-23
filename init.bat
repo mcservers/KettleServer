98ce72c0159038db4bcbb2b14c86eb0cb7701585
Joseph Robinson <smartech56@outlook.com>
2019-07-22 21:21:30 -0700

    added unzip to choco dependency

--- init.bat
+++ init.bat
@@ -1,6 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install git jre8 wget
+choco install git jre8 wget unzip
 
 echo true > initialized
 
