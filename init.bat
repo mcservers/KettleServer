18d34283411799d5d88e72d6d16af830ccea6d18
Joseph Robinson <smartech56@outlook.com>
2019-07-22 21:19:03 -0700

    added wget as a choco dependency

--- init.bat
+++ init.bat
@@ -1,6 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install git jre8
+choco install git jre8 wget
 
 echo true > initialized
 
